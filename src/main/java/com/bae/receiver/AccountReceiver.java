6a1c65239a72f980edb20dc11a362bcd3ff3c9fc
Harry Buck <harry-buck@hotmail.co.uk>
2019-08-08 13:08:31 +0100

    Refactored to make use of jackson objectmapper

--- src/main/java/com/bae/receiver/AccountReceiver.java
+++ src/main/java/com/bae/receiver/AccountReceiver.java
@@ -1,21 +1,33 @@
 package com.bae.receiver;
 
+import java.io.IOException;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import com.bae.entity.Customer;
 import com.bae.repository.MongoAccountRepo;
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Component
 public class AccountReceiver {
 
-	@Autowired
 	private MongoAccountRepo repo;
+	private ObjectMapper objectMapper;
+
+	@Autowired
+	public AccountReceiver(MongoAccountRepo repo, ObjectMapper objectMapper) {
+		this.repo = repo;
+		this.objectMapper = objectMapper;
+	}
 
 	@JmsListener(destination = "AccountQueue")
-	public void receiveMessage(Customer account) {
-		repo.insert(account);
+	public void receiveMessage(String account) throws JsonParseException, JsonMappingException, IOException {
+		Customer c = objectMapper.readValue(account, Customer.class);
+		repo.insert(c);
 	}
 
 }
